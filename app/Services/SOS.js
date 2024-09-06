1062033dbc940b548a68f94603e4a7ce1d3b396a
saisiddharthp9 <saisiddharthp8@gmail.com>
2024-09-06 14:14:35 +0530

    commit

--- app/Services/SOS.js
+++ app/Services/SOS.js
@@ -1,14 +1,39 @@
 import { Text, View } from "react-native";
 import React from "react";
+import { StyleSheet, Button } from "react-native";
 
 const s = require("../../styles");
 
 const Distress = () => {
+  const [message, setMessage] = useState("");
+
   return (
     <View style={s.container}>
-      <Text>Distress Page</Text>
+      <View style={styles.title}>
+        <Text>Distress Page</Text>
+      </View>
+      <View style={styles.distressMsg}>
+        <Text>Tap the button below to trigger a distress call.</Text>
+        <Text>
+          <Button onClick={() => alert("Distress call triggered!")}>
+            Trigger Distress
+          </Button>
+        </Text>
+      </View>
     </View>
   );
 };
 
 export default Distress;
+
+const styles = StyleSheet.create({
+  title: {
+    marginBottom: 20,
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#25292e",
+  },
+  distressMsg: {
+    padding: 20,
+  },
+});
