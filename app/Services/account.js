deebe4241627dfb9cad3f6361c39d8e80e2060dd
saisiddharthp9 <saisiddharthp8@gmail.com>
2024-09-10 11:06:48 +0530

    commit

--- app/Services/account.js
+++ app/Services/account.js
@@ -9,9 +9,19 @@ const Account = () => {
       <View style={{ boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px" }}>
         <Text style={styles.title}>Account Profile</Text>
       </View>
+      <View style={styles.photoContainer}>
+        <View style={styles.photo}></View>
+      </View>
       <View>
-        <Text>Userame :</Text>
-        <Text>Email :</Text>
+        <Text style={styles.details}>Userame :</Text>
+
+        <Text style={styles.details}>Email :</Text>
+
+        <Text style={styles.details}>Occupation :</Text>
+
+        <Text style={styles.details}>Address :</Text>
+
+        <Text style={styles.details}>Phone Number :</Text>
       </View>
     </View>
   );
@@ -25,5 +35,27 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "#25292e",
     padding: 10,
+  },
+  photoContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  photo: {
+    border: "1px solid gray",
+    borderRadius: 50,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    overflow: "hidden",
+    marginLeft: 20,
+    marginTop: 20,
+    justifyContent: "center",
+  },
+  details: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "#25292e",
+    padding: 10,
+    marginTop: 10,
   },
 });
