2be7a3a4dc82b8e6b8c8a8714b6e560607811fc6
saisiddharthp9 <saisiddharthp8@gmail.com>
2024-09-20 11:09:55 +0530

    commit

--- app/Services/SOS.js
+++ app/Services/SOS.js
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput } from "react-native";
 import React, { useState } from "react";
 import { StyleSheet, Button } from "react-native";
 import { Platform } from "react-native";
@@ -10,10 +10,19 @@ const Distress = () => {
 
   return (
     <View style={s.container}>
-      <View style={styles.title}>
-        <Text>Distress Page</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Distress Page</Text>
       </View>
-      <View></View>
+      <View>
+        <Text style={styles.distressMsg}>Enter distress message:</Text>
+        <TextInput
+          style={{ height: 100, borderColor: "gray", borderWidth: 1 }}
+          multiline={true}
+          onChangeText={(text) => setMessage(text)}
+        />
+        <br />
+        <Button title="Send Distress" onPress={() => console.log(message)} />
+      </View>
     </View>
   );
 };
@@ -21,8 +30,13 @@ const Distress = () => {
 export default Distress;
 
 const styles = StyleSheet.create({
-  title: {
+  titleContainer: {
     marginBottom: 20,
+    padding: 10,
+    boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
+    flexDirection: "row",
+  },
+  title: {
     fontSize: 20,
     fontWeight: "bold",
     color: "#25292e",
