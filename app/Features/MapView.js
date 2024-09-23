5c88f26ad4e1a8863070b10f65e268391b5c8483
saisiddharthp9 <saisiddharthp8@gmail.com>
2024-09-23 15:47:33 +0530

    commit

--- app/Features/MapView.js
+++ app/Features/MapView.js
@@ -1,25 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text, ActivityIndicator } from "react-native";
 import { mappls, mappls_plugin } from "mappls-web-maps";
 
 const mapClassObject = new mappls();
 const mapPluginObject = new mappls_plugin();
-var pts = [
-  { lat: 28.55108, lng: 77.26913 },
-  { lat: 28.55106, lng: 77.26906 },
-  { lat: 28.55105, lng: 77.26897 },
-  { lat: 28.55101, lng: 77.26872 },
-  { lat: 28.55099, lng: 77.26849 },
-  { lat: 28.55097, lng: 77.26831 },
-  { lat: 28.55093, lng: 77.26794 },
-  { lat: 28.55089, lng: 77.2676 },
-  { lat: 28.55123, lng: 77.26756 },
-  { lat: 28.55145, lng: 77.26758 },
-  { lat: 28.55168, lng: 77.26758 },
-  { lat: 28.55175, lng: 77.26759 },
-  { lat: 28.55177, lng: 77.26755 },
-  { lat: 28.55179, lng: 77.26753 },
-];
 
 const MapComponent = () => {
   const mapRef = useRef(null);
@@ -28,30 +12,39 @@ const MapComponent = () => {
   const loadObject = {
     map: true,
     layer: "raster", // Optional Default Vector
-    version: "3.0", // // Optional, other version 3.5 also available with CSP headers
+    version: "3.4", // // Optional, other version 3.5 also available with CSP headers
     libraries: ["polydraw"], //Optional for Polydraw and airspaceLayers
     plugins: ["direction"], // Optional for All the plugins
   };
 
   useEffect(() => {
-    mapClassObject.initialize(
-      "c7850c2e067688d30e2fadcd4793935c",
-      loadObject,
-      () => {
-        const newMap = mapClassObject.Map({
-          id: "map",
-          properties: {
-            center: [28.633, 77.2194],
-            zoom: 4,
-          },
-        });
+    const initializeMap = async () => {
+      await mapClassObject.initialize(
+        "c7850c2e067688d30e2fadcd4793935c",
+        loadObject,
+        () => {
+          const newMap = mapClassObject.Map({
+            id: "map",
+            properties: {
+              center: [28.544, 77.5454],
+              zoom: 5,
+              traffic: true,
+              Geolocation: true,
+              clickableIcons: true,
+              fullscreenControl: true,
+            },
+          });
 
-        newMap.on("load", () => {
-          setMapLoaded(true);
-        });
-        mapRef.current = newMap;
-      }
-    );
+          newMap.on("load", () => {
+            setMapLoaded(true);
+          });
+          mapRef.current = newMap;
+        }
+      );
+    };
+
+    initializeMap();
+
     return () => {
       if (mapRef.current) {
         mapRef.current.remove();
@@ -61,10 +54,16 @@ const MapComponent = () => {
 
   return (
     <View id="map" style={styles.map}>
-      {isMapLoaded}
+      {isMapLoaded ? null : <LoadingIndicator />}
     </View>
   );
 };
+
+const LoadingIndicator = () => (
+  <View style={styles.loadingContainer}>
+    <ActivityIndicator size="large" color="#00ff00" />
+  </View>
+);
 
 export default MapComponent;
 
@@ -72,8 +71,13 @@ const styles = StyleSheet.create({
   map: {
     flex: 1,
     margin: "auto",
-    borderRadius: 15,
+    borderRadius: 10,
     width: "90%",
     height: 90,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
--- app/login.js
+++ app/login.js
@@ -10,6 +10,11 @@ import * as Facebook from "expo-auth-session/providers/facebook";
 import { ResponseType } from "expo-auth-session";
 
 export default function Login() {
+  const SUPABASE_KEY =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwYnFwcWh2eGxtZ3h0dmJ6bG5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYxMjQyODYsImV4cCI6MjA0MTcwMDI4Nn0.-8wk0rPu-mehOwAdmoYgtPqlzv84n8ockeeh4Lkf4xA";
+  const supabaseUrl = "https://rpbqpqhvxlmgxtvbzlnn.supabase.co";
+  const supabaseKey = process.env.SUPABASE_KEY;
+  const supabase = createClient(supabaseUrl, supabaseKey);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("");
